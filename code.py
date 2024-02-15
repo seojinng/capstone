9293f80b6f233338ea15dfe3e84a5bd9826a3930
seojinng <113421098+seojinng@users.noreply.github.com>
2024-02-15 13:53:02 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,8 @@
 import streamlit as st
 import qrcode
-from PIL import Image
 
 url = "https://uscapston.streamlit.app/"
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data(url)
-qr.make(fit=True)
+qr_img = qrcode.make(url)
+st.image(qr_img)
 
-
-img = qr.make_image(fill_color="black", back_color="white")
-
-
-#img.save("qrcode.png")
-
-img.show()
