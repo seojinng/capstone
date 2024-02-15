cdeb5181665a2f80cec5ef468a05d7a0311f1827
seojinng <113421098+seojinng@users.noreply.github.com>
2024-02-15 14:33:22 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,24 @@
 import streamlit as st
 import qrcode
+from PIL import Image
+import io
 
 url = "https://uscapston.streamlit.app/"
 
-qr_img = qrcode.make(url)
-st.image(qr_img)
+# QR 코드 생성
+qr_code = qrcode.make(url)
+
+# PIL 이미지로 변환
+img_pil = qr_code.get_image()
+
+# 이미지를 바이트로 변환
+img_byte_arr = io.BytesIO()
+img_pil.save(img_byte_arr, format='PNG')
+img_byte_arr = img_byte_arr.getvalue()
+
+# 스트림릿 앱에 이미지 표시
+st.image(img_byte_arr, use_column_width=True)
+
 
 
 
