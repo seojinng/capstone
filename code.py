72a7b0acd13f12d5fb9847436ded60f27c0cb1bf
seojinng <113421098+seojinng@users.noreply.github.com>
2024-02-15 13:51:43 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import qrcode
+from PIL import Image
+
+url = "https://uscapston.streamlit.app/"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
 
 
-st.text('hello')
+img = qr.make_image(fill_color="black", back_color="white")
+
+
+#img.save("qrcode.png")
+
+img.show()
