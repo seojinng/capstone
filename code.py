b379c8577c5999372a9fd93013cffac9841f6fb8
seojinng <113421098+seojinng@users.noreply.github.com>
2024-02-15 14:08:00 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,11 @@
 #pip install streamlit_qrcode
 
 import streamlit as st
-import qrcode
+#import qrcode
+st.write('hello')
 
-url = "https://uscapston.streamlit.app/"
+#url = "https://uscapston.streamlit.app/"
 
-qr_img = qrcode.make(url)
-st.image(qr_img)
+#qr_img = qrcode.make(url)
+#st.image(qr_img)
 
